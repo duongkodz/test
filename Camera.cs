633bdae8a42b6702fd6abad401df8fd78df40fe2
duongkodz <157829293+duongkodz@users.noreply.github.com>
2024-02-06 22:45:30 +0700

    Add files via upload

---
+++ Camera.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Drawing;
+using System.Windows.Forms;
+using Sentech.GenApiDotNET;
+namespace VISION_STATION_1
+{
+    public class Camera
+    {
+        #region Checking Position on Image
+        static Rect BoundingRect = new Rect();
+        static float MasterX = 0;
+        static float MasterY = 0;
+        static float MasterTheta = 0;
+        public static void GetImage(System.Drawing.Image Image)
+        {
+            Bitmap bitmap = (Bitmap)Image;
+            Mat PictureBoxImage = BitmapConverter.ToMat(bitmap);
+            Global_Var.MatRef = PictureBoxImage;
+        }
+        public static Mat PreProcessing(Mat image, int k)
+        {
+            Mat CloneImage = image.Clone();
+            Cv2.CvtColor(CloneImage, CloneImage, ColorConversionCodes.BGR2GRAY);
+            Mat BlurImage = CloneImage.GaussianBlur(new OpenCvSharp.Size(k, k), 0);
+            /*Using threshold to make binary image*/
+            double MaxVal = 255;
+            double Thresh = 127;
+            Mat Convert_Image = new Mat();
+            Cv2.Threshold(BlurImage, Convert_Image, Thresh, MaxVal, ThresholdTypes.Binary);
+            return Convert_Image;
+        }
+        public static Mat[] ContourDetect(Mat image, double minContourArea, double maxContourArea)
+        {
+            Mat[] contours;
+            Mat hierarchy = new Mat();
+            image = PreProcessing(image, 15);
+            image.FindContours(out contours, hierarchy, RetrievalModes.List, ContourApproximationModes.ApproxNone);
+            Mat[] filteredContours = Array.FindAll(contours, c => (Cv2.ContourArea(c) > minContourArea && Cv2.ContourArea(c) < maxContourArea));
+            return filteredContours;
+        }
+        public static bool FindPosition(Mat[] contours)
+        {
+            if (contours.Length > 0)
+            {
+                BoundingRect = Cv2.BoundingRect(contours[0]);
+                return true;
+            }
+            else 
+                return false;
+        }
+        public static RotatedRect RoiProcess(Mat image,Mat contours)
+        {
+            var rotatedRect = Cv2.MinAreaRect(contours);
+            var boxPoints = Cv2.BoxPoints(rotatedRect);
+            var box = boxPoints.Select(p => new OpenCvSharp.Point((int)p.X, (int)p.Y)).ToArray();
+            image.Polylines(new OpenCvSharp.Point[][] { box }, isClosed: true, color: Scalar.Red, thickness: 5);
+            Cv2.Circle(image, (int)rotatedRect.Center.X, (int)rotatedRect.Center.Y, 8, Scalar.Red, -1);
+            return rotatedRect;
+            
+        }
+        public static float[] PositionData(RotatedRect rotatedRect)
+        {
+            float[] Data = new float[3];
+            //float PixelX = Data[0] 
+            //float PixelY = Data[1] 
+            //float PixelTheta = Data[1] 
+            
+            Data[0] = MasterX- rotatedRect.Center.X;
+            Data[1] = MasterY- rotatedRect.Center.Y;
+            Data[2] = MasterTheta+ rotatedRect.Angle;
+            MessageBox.Show(Data[0].ToString() + "//" + Data[1].ToString() + "//" + Data[2].ToString());
+            return Data;
+        }
+        public bool ImageProcessing()
+        {
+            GetImage(Global_Var.picRef);
+            Mat[] contours =ContourDetect(Global_Var.MatRef, 1000, 3145728);
+            if(FindPosition(contours)==true)
+            {
+                var rotatedRect = RoiProcess(Global_Var.MatRef, contours[0]);
+                PositionData(rotatedRect);
+                return true;
+            }    
+            else { return false; }
+        }
+        #endregion
+        #region QR checking 
+        public static Mat[] QRCodeProcessing(Mat image,RotatedRect rotatedRect)
+        {
+
+            Mat CloneImage = image.Clone();
+            PreProcessing(CloneImage, 3);
+            float[] Data = PositionData(rotatedRect);
+            ///////////////////////////////////////////////////////////////////////
+            OpenCvSharp.Point[] CenterPoint_ver = new OpenCvSharp.Point[2];
+
+            double calibValue = 0.183;
+            double RealDistance = 50;
+            double pixelDistance = RealDistance / calibValue;
+            int rectangleWidth = 70;
+            int rectangleHeight = 40;
+            if (Data[2] > 45)  
+            {
+                CenterPoint_ver[0] = new OpenCvSharp.Point((float)(Data[0] - pixelDistance * (float)Math.Asin(90 - Data[2])), (float)(Data[1] - pixelDistance * (float)Math.Acos(90 - Data[2])));
+                CenterPoint_ver[1] = new OpenCvSharp.Point((float)(Data[0] + pixelDistance * (float)Math.Asin(90 - Data[2])), (float)(Data[1] + pixelDistance * (float)Math.Acos(90 - Data[2])));
+            }
+            else
+            {
+                CenterPoint_ver[0] = new OpenCvSharp.Point((float)(Data[0] + pixelDistance * (float)Math.Asin(90 - Data[2])), (float)(Data[1] - pixelDistance * (float)Math.Acos(90 - Data[2])));
+                CenterPoint_ver[1] = new OpenCvSharp.Point((float)(Data[0] - pixelDistance * (float)Math.Asin(90 - Data[2])), (float)(Data[1] + pixelDistance * (float)Math.Acos(90 - Data[2])));
+            }
+            Mat[] croppedImage_Clone = new Mat[2];
+            for (int i = 0; i < 2; i++)
+            {
+                Mat rotationMatrix = new Mat();
+                if (Data[2] > 45)
+                {
+                    rotationMatrix = Cv2.GetRotationMatrix2D(new Point2f(CenterPoint_ver[i].X, CenterPoint_ver[i].Y), -(90 - Data[2]), 1.0);
+                }
+                else
+                {
+                    rotationMatrix = Cv2.GetRotationMatrix2D(new Point2f(CenterPoint_ver[i].X, CenterPoint_ver[i].Y), Data[2], 1.0);
+                }
+                Mat rotatedImage = new Mat();
+
+                // Xoay ảnh
+                Cv2.WarpAffine(CloneImage, rotatedImage, rotationMatrix, CloneImage.Size());
+
+                int x = CenterPoint_ver[i].X - rectangleWidth / 2;
+                int y = CenterPoint_ver[i].Y - rectangleHeight / 2;
+                Rect roi = new Rect(x, y, rectangleWidth, rectangleHeight);
+                Mat croppedImage = new Mat(rotatedImage, roi);
+
+                croppedImage_Clone[i] = croppedImage.Clone();
+            }
+
+            return croppedImage_Clone;
+        }
+        public static int RoiCompare(Mat image, RotatedRect rotatedRect)
+        {
+            Mat CloneImage = image.Clone();
+            Mat[] mat_clone = new Mat[2];
+            mat_clone = QRCodeProcessing(image, rotatedRect);
+            Mat Darkmat0 = PreProcessing(mat_clone[0], 3);
+            Mat Darkmat1 = PreProcessing(mat_clone[1], 3);
+
+            int a = Cv2.CountNonZero(Darkmat0);
+            int b = Cv2.CountNonZero(Darkmat1);
+            long c = Darkmat0.Total();
+
+            if (a > b && a == c) 
+            {
+                return 1;
+            }
+            if (a < b && b == c) 
+            {
+                return 2;
+            }
+            else
+                return 3;
+        }
+        #endregion
+        #region  Calculate Real Point
+        public static void OffsetToRobot(float[] Data)
+        {
+            /*Calculate deltaX and deltaY*/
+            //float deltaX = Data[0] ;
+            //float deltaY = Data[1] ;
+            /*Convert Pixel to MM*/
+            Global_Var.RealCenter.X = Data[0] * 0.2131 * 1000;
+            Global_Var.RealCenter.Y = Data[1] * 0.2082 * 1000;
+            Global_Var.RealAngle = Data[2] * 10000;
+        }
+        #endregion
+    }
+}
---
+++ ImageProcessing.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Drawing;
+using ZXing;
+using System.Threading;
+namespace VISION_STATION_4
+{
+    public class ImageProcessing
+    {
+        public static OpenCvSharp.Point[] Vertex = new OpenCvSharp.Point[4];
+        double pixel_cm_ratio;
+        public Mat GetMat(System.Drawing.Image Image)
+        {
+            Bitmap bitmap = (Bitmap)Image;
+            Mat PictureBoxImage = BitmapConverter.ToMat(bitmap);
+            return PictureBoxImage;
+        }
+        #region Contour Detection
+        public Mat[] ContourDetect(Mat Image, int MinArea, int MaxArea)
+        {
+            Mat CloneImage = Image.Clone();
+            Cv2.CvtColor(CloneImage, CloneImage, ColorConversionCodes.BGR2GRAY);
+            Mat BlurImage = CloneImage.GaussianBlur(new OpenCvSharp.Size(3, 3), 0);
+            double MaxVal = 255;
+            double Thresh = 127;
+            Mat Convert_Image = new Mat();
+            Cv2.Threshold(BlurImage, Convert_Image, Thresh, MaxVal, ThresholdTypes.Binary);
+            Mat[] contours;
+            Mat hierarchy = new Mat();
+            Cv2.FindContours(Convert_Image, out contours, hierarchy, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
+            Mat[] filteredContours = Array.FindAll(contours, c => (Cv2.ContourArea(c) > MinArea && Cv2.ContourArea(c) < MaxArea));
+            return filteredContours;
+        }
+        #endregion
+        #region DetectWorkpiece
+        public void RectangleDetect(Mat Image)
+        {
+            Mat[] contours = ContourDetect(Image, 1000000, 2000000);
+            var rotatedRect = Cv2.MinAreaRect(contours[0]);
+            var boxPoints = Cv2.BoxPoints(rotatedRect);
+            var box = boxPoints.Select(p => new OpenCvSharp.Point((int)p.X, (int)p.Y)).ToArray();
+
+            Image.Polylines(new OpenCvSharp.Point[][] { box }, isClosed: true, color: Scalar.Red, thickness: 5);
+            Cv2.Circle(Image, (int)rotatedRect.Center.X, (int)rotatedRect.Center.Y, 8, Scalar.Red, -1);
+            Global_Var.Data_X = (int)((rotatedRect.Center.X - Global_Var.MasterPointX) * pixel_cm_ratio);
+            Global_Var.Data_Y = (int)((rotatedRect.Center.Y - Global_Var.MasterPointY) * pixel_cm_ratio);
+            Vertex = box;
+        }
+        #endregion
+        #region QR
+        public string QRReader(Bitmap bitmap)
+        {
+            BarcodeReader barcodeReader = new BarcodeReader();
+            var result = barcodeReader.Decode(bitmap);
+            string qrCodeContent = null;
+            if (result != null)
+            {
+                qrCodeContent = result.Text;
+            }
+            else
+                qrCodeContent = null;
+            return qrCodeContent;
+        }
+        #endregion
+        #region Contour
+        public bool ContourLength(Mat Image, int MaxLength)
+        {
+            Mat[] ROIcontours = ContourDetect(Image, 1000000, 1500000);
+            Rect rect = Cv2.BoundingRect(ROIcontours[0]);
+            Rect RoiRect = new Rect(rect.Location, rect.Size);
+            Mat ROIMat = new Mat(Image, RoiRect);
+            Mat[] contours = ContourDetect(ROIMat, 60000, 90000);
+            double Length = Cv2.ArcLength(contours[0], closed: true);
+            if (Length >= MaxLength)  //max 7955, type1
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        #endregion        
+        #region screw check
+        public Mat imageROI(Mat Image, OpenCvSharp.Point StartPoint, OpenCvSharp.Size Size)
+        {
+            Rect rect = new Rect(StartPoint, Size);
+            Mat ROIMat = new Mat(Image, rect);
+            Mat GrayMat = ROIMat.CvtColor(ColorConversionCodes.BGR2GRAY);
+            return GrayMat;
+        }
+        public int LightPixelCheck(Mat Image, int val)
+        {
+            double MaxVal = 255;
+            double Thresh = 100;
+            Mat Convert_Image = new Mat();
+            Cv2.Threshold(Image, Convert_Image, Thresh, MaxVal, ThresholdTypes.Binary);
+            int a = Cv2.CountNonZero(Convert_Image);
+            if (a > val)  
+            {
+                return 1;
+            }
+            else
+                return 0;
+        }
+        public int[] CheckScrewType1(Mat Image)
+        {
+            int[] Check = new int[4];
+            Mat matRoi1 = imageROI(Image, new OpenCvSharp.Point(306, 149), new OpenCvSharp.Size(66, 66));
+            Mat matRoi3 = imageROI(Image, new OpenCvSharp.Point(835, 160), new OpenCvSharp.Size(66, 66));
+            Mat matRoi5 = imageROI(Image, new OpenCvSharp.Point(830, 1275), new OpenCvSharp.Size(66, 66));
+            Mat matRoi7 = imageROI(Image, new OpenCvSharp.Point(314, 1289), new OpenCvSharp.Size(66, 66));
+
+            Check[0] = LightPixelCheck(matRoi1, 1800);
+            Check[1] = LightPixelCheck(matRoi3, 1800);
+            Check[2] = LightPixelCheck(matRoi5, 1500);
+            Check[3] = LightPixelCheck(matRoi7, 2500);
+            return Check;
+        }
+        public int[] CheckScrewType2(Mat Image)
+        {
+            int[] Check = new int[4];
+            Mat matRoi2 = imageROI(Image, new OpenCvSharp.Point(575, 172), new OpenCvSharp.Size(66, 66));
+            Mat matRoi4 = imageROI(Image, new OpenCvSharp.Point(820, 720), new OpenCvSharp.Size(66, 66));
+            Mat matRoi6 = imageROI(Image, new OpenCvSharp.Point(576, 1263), new OpenCvSharp.Size(66, 66));
+            Mat matRoi8 = imageROI(Image, new OpenCvSharp.Point(333, 716), new OpenCvSharp.Size(66, 66));
+
+            Check[0] = LightPixelCheck(matRoi2, 2000);
+            Check[1] = LightPixelCheck(matRoi4, 2000);
+            Check[2] = LightPixelCheck(matRoi6, 2000);
+            Check[3] = LightPixelCheck(matRoi8, 2000);
+            return Check;
+        }
+        public int ScrewCount(Mat Image, int[] check)
+        {
+            int count = 0;
+            if (Global_Var.type == 1)
+            {
+                check = CheckScrewType1(Image);
+            }
+            else
+            {
+                check = CheckScrewType2(Image);
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (check[i] == 1)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        #endregion
+
+    }
+}
+
